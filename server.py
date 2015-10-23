e447513e5bf4e500ede527669031c186d9671bfc
Sumukh Sridhara <sumukh1@gmail.com>
2015-10-23 13:10:36 -0700

    update layout and server to handle caldining

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from caldining import get_menu_items
 
 app = Flask(__name__)
+
+@app.route('/menu/<hall>')
+def fetch_menu(hall):
+	name = hall
+	items = get_menu_items(6)
+	return render_template('menu.html', items=items, name=name)
 
 @app.route('/')
 def home():
